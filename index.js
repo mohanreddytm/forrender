09a8242ed899a629e9d205bec463ed8715a58fe6
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-01-25 15:32:30 +0530

    added major things

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 const path = require("path");
 
@@ -21,9 +22,9 @@ const initializeDbAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on port ${PORT}`);
+    });    
   } catch (error) {
     console.log(`DB Error: ${error.message}`);
     process.exit(1);
